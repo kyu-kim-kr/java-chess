e861fc35038a8709b4d4da4d6dc0daaea417a50a
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-02-24 19:45:58 +1300

    Remove unnecessary test code and add new test code
    - 객체에 직접 접근할 수 없으므로 쓸모 없는 테스트코드
    
    - Character의 toUpperCase를 사용하여 코드를 간결화 할 수 있었다. 그에 따라서, 잘 작동하는지 확인해볼 필요가 있음.

--- src/test/java/net/honux/chess/pieces/PieceTest.java
+++ src/test/java/net/honux/chess/pieces/PieceTest.java
@@ -8,14 +8,8 @@ import static org.assertj.core.api.Assertions.*;
 class PieceTest {
 
     @Test
-    void Piece의_컬러와_화면에_출력할_Representation이_각_컬러에_맞는지_확인한다() {
-        verifyPiece(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.White.PAWN.representation);
-        verifyPiece(Piece.createBlackPawn(), Piece.BLACK_COLOR, Piece.Black.PAWN.representation);
-    }
-
-    private void verifyPiece(final Piece piece, final String color, final char representation) {
-        assertThat(piece.getColor()).isEqualTo(color);
-        assertThat(piece.getRepresentation()).isEqualTo(representation);
+    void 컬러에따라서_representaion이_대소문자_변경잘됐는지_확인 () {
+        assertThat(Piece.createBlackBishop().getRepresentation()).isEqualTo('B');
     }
 
     @Test
