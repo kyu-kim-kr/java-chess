c4044828efdb3d681c402c68fd1af47f61571d18
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-02-25 08:49:46 +1300

    Verify pieces with Type
    - 모든 정적 팩토리 메서드들의 타입이 잘들어가는지 확인합니다

--- src/test/java/net/honux/chess/pieces/PieceTest.java
+++ src/test/java/net/honux/chess/pieces/PieceTest.java
@@ -5,7 +5,32 @@ import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.*;
 
+import net.honux.chess.pieces.Piece.Type;
+
 class PieceTest {
+
+    @Test
+    public void blank말을포함하여_체스말을_생성하는_모든메서드들_타입이매치하는지확인() {
+        verifyPiece(Piece.createWhitePawn(), Piece.createBlackPawn(), Type.PAWN);
+        verifyPiece(Piece.createWhiteKnight(), Piece.createBlackKnight(), Type.KNIGHT);
+        verifyPiece(Piece.createWhiteRook(), Piece.createBlackRook(), Type.ROOK);
+        verifyPiece(Piece.createWhiteBishop(), Piece.createBlackBishop(), Type.BISHOP);
+        verifyPiece(Piece.createWhiteQueen(), Piece.createBlackQueen(), Type.QUEEN);
+        verifyPiece(Piece.createWhiteKing(), Piece.createBlackKing(), Type.KING);
+
+        Piece blank = Piece.createBlank();
+        assertThat(blank.isWhite()).isFalse();
+        assertThat(blank.isBlack()).isFalse();
+        assertThat(blank.getType()).isEqualTo(Type.NO_PIECE);
+    }
+
+    private void verifyPiece(final Piece whitePiece, final Piece blackPiece, final Type type) {
+        assertThat(whitePiece.isWhite()).isTrue();
+        assertThat(whitePiece.getType()).isEqualTo(type);
+
+        assertThat(blackPiece.isBlack()).isTrue();
+        assertThat(blackPiece.getType()).isEqualTo(type);
+    }
 
     @Test
     void 컬러에따라서_representaion이_대소문자_변경잘됐는지_확인 () {
