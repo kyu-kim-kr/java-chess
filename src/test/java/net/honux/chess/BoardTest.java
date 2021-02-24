4cb69dc9d349f8896aa3acf72f8df28ee11d5033
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-02-24 18:41:13 +1300

    Remove unnecessary test code
    - 지우려는 테스트 코드가 이미 새로 만들었떤 전체기물의 상태를 볼수있는 체스판구현 테스트로 확인할 수 있습니다.

--- src/test/java/net/honux/chess/BoardTest.java
+++ src/test/java/net/honux/chess/BoardTest.java
@@ -16,19 +16,6 @@ class BoardTest {
     }
 
     @Test
-    void 보드에_폰이_색깔별로_잘_들어가는지_확인한다() {
-        verifyBoard(Piece.WHITE_COLOR, Piece.White.PAWN.representation, 0);
-        verifyBoard(Piece.BLACK_COLOR, Piece.Black.PAWN.representation, 0);
-    }
-
-    private void verifyBoard(String color, char representation, int index) {
-        Piece pawnColor = new Piece(color, representation);
-        board.add(pawnColor);
-        assertThat(board.size(pawnColor)).isEqualTo(index + 1);
-        assertThat(board.findPawn(pawnColor, index)).isEqualTo(pawnColor);
-    }
-
-    @Test
     void 전체기물의_상태를_볼수있는_체스판구현_테스트() {
         board.initialize();
         assertThat(board.pieceCount()).isEqualTo(32);
